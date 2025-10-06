bb4a0e08cbc7455e232ad139f8bfe66354fb1e1f
paul philips <paulphilips@gmail.com>
2025-10-06 12:34:31 +0530

    Add .gitignore to clean repo (ignore node_modules, dist, target)

---
+++ TravelMate/TravelMate/src/main/java/com/TravelMate/controllers/pageController.java
@@ -0,0 +1,17 @@
+package com.TravelMate.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class pageController {
+
+      @RequestMapping("/home")
+      public String home(){
+
+        System.err.println("hello from page controller");
+        return "home";
+
+      }
+
+}
